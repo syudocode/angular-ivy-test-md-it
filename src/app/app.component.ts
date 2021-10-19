e370fa02478da28a2bad936d3a8db8b3e043c0af
syudocode <syudocode@gmail.com>
2021-10-19 19:39:28 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, VERSION } from '@angular/core';
 import { SafeHtml } from '@angular/platform-browser';
 
 import MarkdownIt from 'markdown-it';
+import MardkwonItContainer from 'markdown-it-container';
 
 @Component({
   selector: 'my-app',
@@ -16,10 +17,46 @@ export class AppComponent {
   // result = mdIt.render('# markdown-it rulezz!');
 
   ngOnInit(): void {
+    const sampleMarkdown = `# markdown-it rulezz!
+
+::: spoiler Click Here
+This is a test note
+:::
+
+::: tip text
+This is another test note
+:::
+    
+    `;
+
     const mdIt = new MarkdownIt();
     // const renderedMd = ;
     // result = this.sanitized.bypassSecurityTrustHtml(value);
-    const renderedMd = mdIt.render('# markdown-it rulezz!');
+    mdIt.use(MardkwonItContainer, 'tip');
+    mdIt.use(MardkwonItContainer, 'spoiler', {
+      validate: function (params) {
+        const theregex = new RegExp(/^spoiler\s+(.*)$/);
+        const regexthing = params.trim().match(theregex);
+        console.log('regexthing', regexthing);
+        return regexthing;
+      },
+
+      render: function (tokens, idx) {
+        var m = tokens[idx].info.trim().match(/^spoiler\s+(.*)$/);
+        console.log('m', m);
+        if (tokens[idx].nesting === 1) {
+          // opening tag
+          return (
+            '<details><summary>' + mdIt.utils.escapeHtml(m[1]) + '</summary>\n'
+          );
+        } else {
+          // closing tag
+          return '</details>\n';
+        }
+      },
+    });
+
+    const renderedMd = mdIt.render(sampleMarkdown);
     console.log(renderedMd);
     this.result = renderedMd as SafeHtml;
   }
