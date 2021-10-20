f113037e401d51c216b9de609af81a6000b31fa7
syudocode <syudocode@gmail.com>
2021-10-19 20:41:27 -0400

    it works :D

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,15 @@ export class AppComponent {
 This is a test note
 :::
 
-::: tip text
+::: alert-note
+This is another test note
+:::
+
+::: note
+This is another test note
+:::
+
+::: note text
 This is another test note
 :::
     
@@ -33,21 +41,40 @@ This is another test note
     // const renderedMd = ;
     // result = this.sanitized.bypassSecurityTrustHtml(value);
     mdIt.use(MardkwonItContainer, 'tip');
-    mdIt.use(MardkwonItContainer, 'spoiler', {
+    mdIt.use(MardkwonItContainer, 'alert-note');
+
+    mdIt.use(MardkwonItContainer, 'note', {
       validate: function (params) {
-        const theregex = new RegExp(/^spoiler\s+(.*)$/);
-        const regexthing = params.trim().match(theregex);
-        console.log('regexthing', regexthing);
-        return regexthing;
+        const regexthing = params.trim().match(new RegExp(/^note/));
+        const regexthing2 = params.trim().match(new RegExp(/^note\s+(.*)/));
+
+        console.log('regexthing2', regexthing2);
+        console.log('regexthing length', regexthing2?.length > 0);
+
+        return regexthing2?.length > 0 ? regexthing2 : regexthing;
       },
 
       render: function (tokens, idx) {
-        var m = tokens[idx].info.trim().match(/^spoiler\s+(.*)$/);
+        const regexthing = new RegExp(/^note/);
+        const regexthing2 = new RegExp(/^note\s+(.*)/);
+
+        var m = tokens[idx].info.trim().match(regexthing);
+        var m2 = tokens[idx].info.trim().match(regexthing2);
+
+        // /^spoiler\s+(.*)$/
+        // console.log('tokens', tokens[idx].nesting);
         console.log('m', m);
+        console.log('m2', m2);
+
         if (tokens[idx].nesting === 1) {
+          // console.log('nullcheckthing', nullcheckthing);
+          // var test = !nullcheckthing ? m[1] : 'test2';
+          // console.log('test', test);
           // opening tag
+          console.log('m1', m[1]);
+          var test = m2?.length > 0 ? m2[1] : m;
           return (
-            '<details><summary>' + mdIt.utils.escapeHtml(m[1]) + '</summary>\n'
+            '<details><summary>' + mdIt.utils.escapeHtml(test) + '</summary>\n'
           );
         } else {
           // closing tag
